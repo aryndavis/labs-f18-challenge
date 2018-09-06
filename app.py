6051774d6bb4dc60b484694da432b514269a890b
aryndavis <arynlynn1@gmail.com>
2018-09-05 22:14:21 -0400

    not quite sure how to proceed, here is an educated guess

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template
-
+import requests
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
+@app.route('/pokemon/<query>', methods=['GET'])
 def main():
-    return render_template('index.html')
-
+	if type(query) == type(''):
+		GET api/v2/ability/query
+		return query + ' has id ' + str(id)
+	if type(query) == type(1):
+		GET api/v2/ability/query
+		return 'The pokemon with id ' + query + ' is ' + str(name)
 
 if __name__ == '__main__':
     app.run()
